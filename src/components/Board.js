c4ad037b14341df329a6d0fd5485b346456d64a8
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-09-09 19:21:16 +0430

    Start project

---
+++ src/components/Board.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "../styles/Board.css";
+
+const Board = () => {
+    return <div>Board</div>
+}
+
+export default Board;
---
+++ src/components/XOApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "../styles/XOApp.css";
+
+const XOApp = () => {
+    return <div>XOApp</div>
+}
+
+export default XOApp;
